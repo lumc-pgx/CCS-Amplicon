b388dabdc9d0fa2c84120fc84f3f815c424f0c2b
Guy Allard <w.g.allard@lumc.nl>
2018-10-23 09:16:49 +0200

    prototype analysis workflow

---
+++ prototype/scripts/workflow_fasta.sh
@@ -0,0 +1,22 @@
+SAMPLE=$1
+
+ngmlr -r ${SAMPLE}.consensus.fa -q ${SAMPLE}.fasta --rg-id foo --rg-sm sm --rg-lb lb > ${SAMPLE}.aligned.sam
+samtools sort ${SAMPLE}.aligned.sam > ${SAMPLE}.aligned.bam
+samtools index ${SAMPLE}.aligned.bam
+bcftools mpileup -f ${SAMPLE}.consensus.fa ${SAMPLE}.aligned.bam | bcftools call -mv -Ob -o ${SAMPLE}.bcf
+bcftools view -i '%QUAL>=50' ${SAMPLE}.bcf > ${SAMPLE}.vcf
+
+#whatshap phase --indels --reference ${SAMPLE}.consensus.fa ${SAMPLE}.vcf ${SAMPLE}.aligned.bam -o ${SAMPLE}.phased.vcf
+whatshap phase --indels ${SAMPLE}.vcf ${SAMPLE}.aligned.bam -o ${SAMPLE}.phased.vcf
+bgzip ${SAMPLE}.phased.vcf
+tabix ${SAMPLE}.phased.vcf.gz
+
+#whatshap haplotag -o ${SAMPLE}.aligned.tagged.bam -r ${SAMPLE}.consensus.fa ${SAMPLE}.phased.vcf.gz ${SAMPLE}.aligned.bam
+whatshap haplotag -o ${SAMPLE}.aligned.tagged.bam ${SAMPLE}.phased.vcf.gz ${SAMPLE}.aligned.bam
+samtools index ${SAMPLE}.aligned.tagged.bam
+
+
+# notes
+# if the unphased vcf contains a single heterozygous variant, whatshap will not
+# phase it. This can be overcome by manually adding the phasing information to the phased.vcf
+# once this is done, haplotag will correctly assign reads based on the phase of the variant.
