f94216e0d133c71e92a61fbb98db601c7edac3c8
Guy Allard <w.g.allard@lumc.nl>
2018-10-23 16:35:36 +0200

    info dict

--- workflow/scripts/baminfo.py
+++ workflow/scripts/baminfo.py
@@ -3,6 +3,15 @@ from __future__ import print_function
 import click
 import pysam
 
+def record_summary(record):
+    return {
+        "id": record.query_name,
+        "np": record.get_tag("np"),
+        "rq": record.get_tag("rq"),
+        "len": record.query_length
+    }
+
+
 @click.command(context_settings=dict(
     ignore_unknown_options=True,
     ),
@@ -10,19 +19,13 @@ import pysam
 )
 @click.argument("ccs_bamfile", type=click.Path(exists=True))
 def cli_handler(ccs_bamfile):
+    fields = ["id", "np", "rq", "len"]
     with pysam.AlignmentFile(ccs_bamfile, "rb", check_sq=False) as infile:
-        print("\t".join(["id", "np", "rq", "len"]))
+        print("\t".join(fields))
         for read in infile.fetch(until_eof=True):
-            print(
-                "\t".join([
-                    str(x) for x in [ 
-                        read.query_name,
-                        read.get_tag("np"),
-                        read.get_tag("rq"),
-                        read.query_length
-                    ]
-                ])
-            )
+            summary = record_summary(read)
+            values = [str(summary[f]) for f in fields]
+            print("\t".join(values))
 
 
 if __name__ == '__main__':
