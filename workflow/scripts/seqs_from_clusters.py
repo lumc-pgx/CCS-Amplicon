e0484457786fd14afeec3d9fe6a6a6f3ebcd79cc
Guy Allard <w.g.allard@lumc.nl>
2018-11-05 17:28:31 +0100

    generate per-cluster fasta sequences

---
+++ workflow/scripts/seqs_from_clusters.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+from __future__ import print_function
+import click
+from Bio import SeqIO
+import json
+
+
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+    ),
+    short_help="generate fasta files for clusters"
+)
+@click.option("--prefix", "-p", type=str, default="cluster",
+              help="prefix for output file names")
+@click.argument("sequence_fasta", type=click.Path(exists=True))
+@click.argument("clusters_json", type=click.Path(exists=True))
+def cli_handler(prefix, sequence_fasta, clusters_json):
+    with open(clusters_json, "r") as infile:
+        clusters = json.load(infile)
+
+    output_files = [open("{}{}.fasta".format(prefix, cluster["cluster"]), "w") for cluster in clusters]
+
+    for i, record in enumerate(SeqIO.parse(sequence_fasta, "fasta")):
+        for j, cluster in enumerate(clusters):
+            if i in cluster["members"]:
+                SeqIO.write(record, output_files[j], "fasta")
+                break
+
+    for f in output_files:
+        f.close()
+
+if __name__ == '__main__':
+    cli_handler()
