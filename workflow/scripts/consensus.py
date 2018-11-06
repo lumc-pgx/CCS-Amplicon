c15df19c44f36b0fea729a53f13d56321f6ebc24
Guy Allard <w.g.allard@lumc.nl>
2018-11-06 09:59:42 +0100

    consensus generation

---
+++ workflow/scripts/consensus.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+from __future__ import print_function
+import click
+from Bio import AlignIO
+from Bio.Align import AlignInfo
+
+
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+    ),
+    short_help="generate consensus sequence from MSA"
+)
+@click.option("--fraction", "-f", type=float, default=0.51,
+              help="call consensus for bases with frequency >= fraction at a position")
+@click.option("--seqid", "-s", type=str, default="consensus",
+              help="fasta sequence id to use for consensus sequence")
+@click.argument("msa_fasta", type=click.Path(exists=True))
+def cli_handler(fraction, seqid, msa_fasta):
+    msa = AlignIO.read(msa_fasta, "fasta")
+    summary = AlignInfo.SummaryInfo(msa)
+    consensus = str(summary.gap_consensus(fraction, require_multiple=True, ambiguous='')).replace("-", "")
+
+    print(">{}".format(seqid))
+    print(consensus)
+
+if __name__ == '__main__':
+    cli_handler()
