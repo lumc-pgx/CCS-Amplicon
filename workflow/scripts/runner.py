7a798b7958ae47a83b3dd26390748308d365c66f
Guy Allard <w.g.allard@lumc.nl>
2018-11-08 10:31:42 +0100

    full workflow with wrapper script

--- workflow/scripts/runner.py
+++ workflow/scripts/runner.py
@@ -1,51 +1,80 @@
 #! /usr/bin/env python
+import os
 import click
 from snakemake import snakemake
 
 @click.command(context_settings=dict(
     ignore_unknown_options=False,
     ),
-    short_help="CCS driven amplicon polishing and phasing for PacBio sequencing data"
+    short_help="CCS-driven amplicon phasing and polishing for targetted PacBio sequencing data"
 )
 @click.option("--directory", "-d", type=click.Path(file_okay=False), default=".",
-              help="path to the directory which the program output will be written to."
+              help="Path to the directory which the program output will be written to."
                    "If directory is omitted, output will be written to the current directory")
+@click.option("--prefix", "-p", type=str, default="ccs_amplicon",
+              help="Prefix to use for output file names")
+@click.option("--profile", "-po", type=str,
+              help="The name of the snakemake profile to use when running the workflow."
+                   "Use this profile to control how the workflow is run on your specific compute architecture."
+                   "See https://snakemake.readthedocs.io/en/stable/executable.html?highlight=profile for details")
 @click.option("--min-ccs-length", type=click.IntRange(1, None), default=6000,
-              help="minimum required CCS sequence length")
+              help="Minimum required CCS sequence length")
 @click.option("--max-ccs-length", type=click.IntRange(1, None), default=7000,
-              help="maximum allowed CCS sequence length")
+              help="Maximum allowed CCS sequence length")
 @click.option("--min-ccs-passes", type=click.IntRange(1, None), default=1,
-              help="minimum required CCS passes")
+              help="Minimum required CCS passes")
 @click.option("--min-ccs-qual", type=click.FloatRange(0.0, 1.0), default=0.98,
-              help="minimum required CCS sequence quality")
+              help="Minimum required CCS sequence quality")
 @click.option("--max-homopolymer", type=click.IntRange(1, None), default=2,
-              help="homopolymer runs of length greater than max-homopolymer will be collapsed to this length")
+              help="Homopolymer runs of length greater than max-homopolymer will be collapsed to this length")
 @click.option("--tsne-iterations", type=click.IntRange(1, None), default=5000,
-              help="number of iterations for tSNE")
+              help="Number of iterations for tSNE")
 @click.option("--tsne-rate", type=click.IntRange(1, None), default=50,
               help="tSNE learning rate")
-@click.option("--cluster-percentile", type=click.FloatRange(0.0, 1.0), default=0.8,
-              help="cluster members with a similarity above this percentile are considered to be connected."
-                   "lower values result in fewer, larger clusters.")
+@click.option("--cluster-percentile", type=click.IntRange(0, 100), default=80,
+              help="Cluster members with a similarity above this percentile are considered to be connected."
+                   "Lower values result in fewer, larger clusters.")
 @click.option("--cluster-inflation", type=click.FloatRange(0, None), default=1.4,
-              help="markov clustering inflation parameter."
+              help="Markov clustering inflation parameter."
                    "Higer values result in more clusters")
 @click.option("--max-cluster-passes", type=click.IntRange(1, None), default=500,
               help="Limit the number of sequences output by the cluster step")
 @click.option("--consensus-fraction", type=click.FloatRange(0.0, 1.0), default=0.51,
-              help="frequency of nucleotide at a given position required for rough consensus calling")
+              help="Frequency of nucleotide at a given position required for rough consensus calling")
 @click.option("--min-haplotype-molecules", type=click.IntRange(1, None), default=10,
-              help="minimum number of molecules (CCS sequences) required for a haplotype")
+              help="Minimum number of molecules (CCS sequences) required for a haplotype")
 @click.option("--min-variant-qual", type=click.IntRange(0, None), default=50,
-              help="minimum variant qual score required for a variant to be used for phasing")
+              help="Minimum variant qual score required for a variant to be used for phasing")
 @click.argument("ccs_bam", type=click.Path(exists=True))
 @click.argument("subreads_bam", type=click.Path(exists=True))
-def cli_handler(directory, min_ccs_length, max_ccs_length, min_ccs_passes, min_ccs_qual, max_homopolymer,
+def cli_handler(directory, prefix, min_ccs_length, max_ccs_length, min_ccs_passes, min_ccs_qual, max_homopolymer,
                 tsne_iterations, tsne_rate, cluster_percentile, cluster_inflation, max_cluster_passes,
-                consensus_fraction, min_haplotype_molecules, min_variant_qual,
-                ccs_bamfile, subreads_bamfile,):
-    pass
+                consensus_fraction, min_haplotype_molecules, min_variant_qual, ccs_bam, subreads_bam,):
+    # dict of config values to pass to snakemake
+    config = dict(
+        PREFIX = prefix,
+        MIN_READ_LENGTH = min_ccs_length,
+        MAX_READ_LENGTH = max_ccs_length,
+        MIN_PASSES = min_ccs_passes,
+        MIN_QUAL = min_ccs_qual,
+        MAX_HP_LEN = max_homopolymer,
+        TSNE_ITERS = tsne_iterations,
+        TSNE_LR = tsne_rate,
+        CLUSTER_SIMILARITY_PERCENTILE = cluster_percentile,
+        CLUSTER_INFLATION = cluster_inflation,
+        CLUSTER_COVERAGE_LIMIT = max_cluster_passes,
+        CONSENSUS_FRACTION = consensus_fraction,
+        MIN_HAPLOTYPE_MOLECULES = min_haplotype_molecules,
+        MIN_VARIANT_QUAL = min_variant_qual,
+        CCS_BAM = ccs_bam,
+        SUBREADS_BAM = subreads_bam
+    )
 
+    snakemake(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "../snakefiles/workflow.snake"),
+        workdir = directory,
+        config = config
+    )
 
 if __name__ == "__main__":
     cli_handler()
