61167e80be33f5355d1e7daa87c4c5fb2ffe7d17
Guy Allard <w.g.allard@lumc.nl>
2018-11-16 13:18:33 +0100

    visualize filtered clusters

--- ccs_amplicon/cluster_view.py
+++ ccs_amplicon/cluster_view.py
@@ -11,7 +11,7 @@ from bokeh.models.widgets import Div
 import click
 
 
-def make_plot(clusters, embeddings, info, title):
+def make_plot(clusters, filtered_clusters, embeddings, info, title):
     data = embeddings.copy()
     
     embedded_clusters = [-1 for _ in range(data.shape[0])]
@@ -22,13 +22,20 @@ def make_plot(clusters, embeddings, info, title):
 
     data["cluster"] = embedded_clusters
 
-    datasource = ColumnDataSource({
+    in_filtered = [0 for _ in range(data.shape[0])]
+    for cluster in filtered_clusters:
+        for member in cluster["members"]:
+            in_filtered[member] = 1
+
+
+    datasource1 = ColumnDataSource({
         "x": data[0], 
         "y": data[1],
         "passes": info["np"],
         "len": info["len"],
         "qual": info["rq"],
-        "cluster": [str(c) for c in data["cluster"]]
+        "cluster": [str(c) for c in data["cluster"]],
+        "filter": in_filtered
     })
 
     TOOLTIPS = [
@@ -41,7 +48,9 @@ def make_plot(clusters, embeddings, info, title):
     factors = [str(i) for i in set(data["cluster"])]
 
     p=figure(title=title, tooltips=TOOLTIPS)
-    p.circle(x="x", y="y", source=datasource, size="passes", color=factor_cmap('cluster', palette=viridis(max(2, len(factors))), factors=factors))
+    c = p.circle(x="x", y="y", source=datasource1, size="passes",
+                 color=factor_cmap('cluster', palette=viridis(max(2, len(factors))), factors=factors),
+                 alpha=0.5, line_alpha="filter")
 
     return p
 
@@ -54,12 +63,16 @@ def make_plot(clusters, embeddings, info, title):
 @click.option("--title", "-t", type=str, default="Clusters",
               help="Title for the plot")
 @click.argument("cluster_file", type=click.Path(exists=True))
+@click.argument("filtered_cluster_file", type=click.Path(exists=True))
 @click.argument("embedding_file", type=click.Path(exists=True))
 @click.argument("info_file", type=click.Path(exists=True))
 @click.argument("plot_file", type=click.Path())
-def cli_handler(title, cluster_file, embedding_file, info_file, plot_file):
+def cli_handler(title, cluster_file, filtered_cluster_file, embedding_file, info_file, plot_file):
     with open(cluster_file, "r") as infile:
         clusters = json.load(infile)
+
+    with open(filtered_cluster_file, "r") as infile:
+        filtered_clusters = json.load(infile)
 
     try:
         embeddings = pd.read_csv(embedding_file, sep="\t", header=None)
@@ -70,7 +83,7 @@ def cli_handler(title, cluster_file, embedding_file, info_file, plot_file):
     output_file(plot_file, title="Cluster report", mode="inline")
 
     if len(clusters) > 0:
-        figure = make_plot(clusters, embeddings, bam_info, title)
+        figure = make_plot(clusters, filtered_clusters, embeddings, bam_info, title)
     else:
         figure = Div(text="No Data")
 
