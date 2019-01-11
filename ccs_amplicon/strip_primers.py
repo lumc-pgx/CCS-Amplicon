ff4d801f5e0dfd60016811534cd8d4a439e9853d
Guy Allard <w.g.allard@lumc.nl>
2019-01-11 11:55:08 +0100

    refactor

--- ccs_amplicon/strip_primers.py
+++ ccs_amplicon/strip_primers.py
@@ -17,58 +17,112 @@ R_RC_PRIMER = -2
 NULL_PRIMER = 0
 NULL_SCORE = -1
 
+# degenerate nucleotides
+IUPAC_EQUALITIES = [
+    ("R", "A"), ("R", "G"),
+    ("Y", "C"), ("Y", "T"),
+    ("M", "A"), ("M", "C"),
+    ("K", "G"), ("K", "T"),
+    ("S", "C"), ("S", "G"),
+    ("W", "A"), ("W", "T"),
+    ("H", "A"), ("H", "C"), ("H", "T"),
+    ("B", "C"), ("B", "G"), ("B", "T"),
+    ("V", "A"), ("V", "C"), ("V", "G"),
+    ("D", "A"), ("D", "G"), ("D", "T"),
+    ("N", "A"), ("N", "C"), ("N", "G"), ("N", "T")
+]
+
+
+def align(query, target):
+    """
+    Align query sequence to target sequence using edlib in prefix mode.
+    IUPAC codes for degenerate nucleotides are supported.
+    :param query: The query sequence
+    :type query: string
+    :param target: The target sequence
+    :type target: string
+    :return: The alignment
+    :rtype: edlib alignment dictionary
+    """
+    return edlib.align(query, target, mode="SHW", additionalEqualities=IUPAC_EQUALITIES)
+
+
+def find_primers(f, r, target, max_dist):
+    seq = target.upper()
+    seq_rc = reverse_complement(seq)
+
+    # initially, no primers found
+    primers_found = [NULL_PRIMER, NULL_PRIMER]
+    primer_scores = [NULL_SCORE, NULL_SCORE]
+
+    # align and score
+    f_aln = align(f, seq)        # forward vs seq
+    f_rc_aln = align(f, seq_rc)  # forward vs revcomp(seq)
+
+    f_score = f_aln["editDistance"]
+    f_rc_score = f_rc_aln["editDistance"]
+
+    r_aln = align(r, seq)        # rev vs seq
+    r_rc_aln = align(r, seq_rc)  # rev vs revcomp(seq)
+
+    r_score = r_aln["editDistance"]
+    r_rc_score = r_rc_aln["editDistance"]
+
+    # start and end positions for trimmed sequence
+    start, end = (0, 0)
+
+    # forward primer match
+    if f_score <= max_dist:
+        primers_found[START_IDX] = F_PRIMER
+        primer_scores[START_IDX] = f_score
+        start = f_aln["locations"][-1][-1] + 1
+
+    # forward primer revcomp match
+    if f_rc_score <= max_dist:
+        primers_found[END_IDX] = F_RC_PRIMER
+        primer_scores[END_IDX] = f_rc_score
+        end = -f_rc_aln["locations"][-1][-1] - 1
+
+    # reverse primer match
+    if r_score <= max_dist:
+        primers_found[START_IDX] = R_PRIMER
+        primer_scores[START_IDX] = r_score
+        start = r_aln["locations"][-1][-1] + 1
+
+    # reverse primer revcomp match
+    if r_rc_score <= max_dist:
+        primers_found[END_IDX] = R_RC_PRIMER
+        primer_scores[END_IDX] = r_rc_score
+        end = -r_rc_aln["locations"][-1][-1] - 1
+
+    return primers_found, primer_scores, start, end
+
+
 def find_and_remove_primers(bam_handle, f, r, max_dist):
+    f = f.upper()
+    r = r.upper()
+
+    # each bam record
     for record in bam_handle.fetch(until_eof=True):
+        # qualities and sequence from bam record
         quals = record.query_qualities
         seq = record.query_sequence
-        seq_rc = reverse_complement(seq)
 
-        primers_found = [NULL_PRIMER, NULL_PRIMER]
-        primer_scores = [NULL_SCORE, NULL_SCORE]
+        primers_found, primer_scores, start, end = find_primers(f, r, seq, max_dist)
 
-        f_aln = edlib.align(f, seq, mode="SHW")
-        f_rc_aln = edlib.align(f, seq_rc, mode="SHW")
-
-        f_score = f_aln["editDistance"]
-        f_rc_score = f_rc_aln["editDistance"]
-
-        r_aln = edlib.align(r, seq, mode="SHW")
-        r_rc_aln = edlib.align(r, seq_rc, mode="SHW")
-
-        r_score = r_aln["editDistance"]
-        r_rc_score = r_rc_aln["editDistance"]
-
-        start, end = (0, 0)
-
-        if f_score <= max_dist:
-            primers_found[START_IDX] = F_PRIMER
-            primer_scores[START_IDX] = f_score
-            start = f_aln["locations"][-1][-1] + 1
-
-        if f_rc_score <= max_dist:
-            primers_found[END_IDX] = F_RC_PRIMER
-            primer_scores[END_IDX] = f_rc_score
-            end = -f_rc_aln["locations"][-1][-1] - 1
-
-        if r_score <= max_dist:
-            primers_found[START_IDX] = R_PRIMER
-            primer_scores[START_IDX] = r_score
-            start = r_aln["locations"][-1][-1] + 1
-
-        if r_rc_score <= max_dist:
-            primers_found[END_IDX] = R_RC_PRIMER
-            primer_scores[END_IDX] = r_rc_score
-            end = -r_rc_aln["locations"][-1][-1] - 1
-
+        # remove primer from start of sequence.
+        trimmed = record.query_sequence
         if start != 0:
-            seq = seq[start:]
+            trimmed = trimmed[start:]
             quals = quals[start:]
 
+        # remove primer from end of sequence
         if end != 0:
-            seq = seq[:end]
+            trimmed = trimmed[:end]
             quals = quals[:end]
 
-        record.query_sequence = seq
+        # update bam record
+        record.query_sequence = trimmed
         record.query_qualities = quals
         record.tags += [("pf", primers_found)]
         record.tags += [("pd", primer_scores)]
@@ -116,10 +170,12 @@ def find_and_remove_primers(bam_handle, f, r, max_dist):
 )
 def cli_handler(input_bamfile, output_bamfile, forward, reverse, max_dist):
 
-    with pysam.AlignmentFile(input_bamfile, "rb", check_sq=False) as input_handle:
-        with pysam.AlignmentFile(output_bamfile, "wb", template=input_handle) as output_handle:
-            records = find_and_remove_primers(input_handle, forward, reverse, max_dist)
-
+    with pysam.AlignmentFile(input_bamfile, "rb",
+                             check_sq=False) as input_handle:
+        with pysam.AlignmentFile(output_bamfile, "wb",
+                                 template=input_handle) as output_handle:
+            records = find_and_remove_primers(input_handle, forward,
+                                              reverse, max_dist)
             for r in records:
                 output_handle.write(r)
 
