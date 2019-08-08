0f4f35c468e6e5f43f4ed037da5238e3ab0bc22f
Guy Allard <w.g.allard@lumc.nl>
2019-08-08 09:44:29 +0200

    updated author info

--- ccs_amplicon/__init__.py
+++ ccs_amplicon/__init__.py
@@ -2,6 +2,6 @@ __version_info__ = ("0", "0", "1", "dev")
 __date__ = "13 Nov 2018"
 
 __version__ = ".".join(__version_info__)
-__author__ = "Guy Allard"
+__author__ = "Guy Allard, LUMC"
 __contact__ = "guyallard01@gmail.com"
-__homepage__ = "https://git.lumc.nl/wgallard/ccs_amplicon"
+__homepage__ = "https://github.com/lumc-pgx/CCS-Amplicon"
